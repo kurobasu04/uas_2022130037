beb756690658342f440d1dfa2addd25781c82183
Yusup Ridwan <joseph.rizvan@gmail.com>
2024-12-05 13:03:12 +0700

    halaman profil user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -13,6 +14,7 @@ class UserController extends Controller
 
     public function index()
     {
-        return view('user.index');
+        $user = Auth::user();
+        return view('user.index', compact('user'));
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,10 +5,17 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('User Dashboard') }}</div>
+                    <div class="card-header">{{ __('User Profile') }}</div>
 
                     <div class="card-body">
-                        {{ __('Welcome, User!') }}
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" class="form-control" id="name" value="{{ $user->name }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" id="email" value="{{ $user->email }}" readonly>
+                        </div>
                     </div>
                 </div>
             </div>
